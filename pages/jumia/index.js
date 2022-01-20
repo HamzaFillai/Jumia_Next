8b3679212a12e3e4cf8c5ae0f475b60037e01545
Your Name <you@example.com>
2022-01-20 11:23:42 +0100

    last commit

--- pages/jumia/index.js
+++ pages/jumia/index.js
@@ -1,12 +1,15 @@
-import React, { useState } from 'react'
+import React, { useState , useEffect} from 'react'
 import styles from "../../styles/Home.module.css"
 import Cookies from 'js-cookie'
 import axios from 'axios'
 
 export default function index() {
 
-    const [ tvs, setTvs ] = useState();
+    const [ tvs, setTvs] = useState();
+    const [ recs, setRecs] = useState();
     const [classe , setClasse] = useState();
+    const [product,setProduct] = useState();
+    const [clustering,setClustering] = useState();
     const date = Date.now();
 
     const logout = () =>
@@ -35,24 +38,52 @@ export default function index() {
 
     const showFitness = () =>
     {
-        axios.get("http://localhost:8081/getFitness").then((response)=>{
+        axios.get("http://localhost:8081/getElectro").then((response)=>{
             setTvs(response.data);
         });
 
-        setClasse("Fitness")
+        setClasse("Electromenage")
+    }
+
+    const showWatchs = () =>
+    {
+        axios.get("http://localhost:8081/getWatchs").then((response)=>{
+            setTvs(response.data);
+        });
+
+        setClasse("Watchs")
+    }
+
+    const showParfum = () =>
+    {
+        axios.get("http://localhost:8081/getParfum").then((response)=>{
+            setTvs(response.data);
+        });
+
+        setClasse("Parfum")
+    }
+
+    const showInfo = () =>
+    {
+        axios.get("http://localhost:8081/getInfo").then((response)=>{
+            setTvs(response.data);
+        });
+
+        setClasse("Informatique")
     }
 
     function List({ items, fallback }) {
         if (!items || items.length === 0) {
           return fallback;
         } else {
-          return items.map(item => {
-            return <div onClick={()=>cluster(item.idProduct)} style={{cursor:"pointer"}} key={item.idProduct}>{item.caracteristique}</div>;
+          return items.map((item,i) => {
+            return <div onClick={()=>cluster(item.idProduct,item.marque)} style={{cursor:"pointer"}} key={item.idProduct}>{item.caracteristique}</div>;
           });
         }
       }
 
-    const cluster = (idProduct) =>{
+    const cluster = (idProduct,marque) =>{
+        setProduct(marque)
         axios.post("http://localhost:8080/api/cluster",
         {
           idUser : Cookies.get('iduser'),
@@ -64,7 +95,73 @@ export default function index() {
         console.log("Sucess")
         })
         .catch((err) => console.log(err));
+
+        if(classe=="Phone")
+        {
+            axios.get("http://localhost:8081/getphones").then((response)=>{
+                setRecs(response.data);
+            });
+        }
+
+        if(classe=="TV")
+        {
+            axios.get("http://localhost:8081/getTvs").then((response)=>{
+                setRecs(response.data);
+               
+            });
+        }
+        
+        if(classe=="Electromenage")
+        {
+            axios.get("http://localhost:8081/getElectro").then((response)=>{
+                setRecs(response.data);
+            });
+        }
+
+        if(classe=="Watchs")
+        {
+            axios.get("http://localhost:8081/getWatchs").then((response)=>{
+                setRecs(response.data);
+            });
+        }
+
+        if(classe=="Parfum")
+        {
+            axios.get("http://localhost:8081/getParfum").then((response)=>{
+                setRecs(response.data);
+            });
+        }
+
+        if(classe=="Informatique")
+        {
+            axios.get("http://localhost:8081/getInfo").then((response)=>{
+                setRecs(response.data);
+            });
+        }
     }
+
+    function Recommand({ items, fallback }) {
+        if (!items || items.length === 0) {
+          return fallback;
+        } else {
+          return items.map((item,i) => {  
+            let c = 0;
+            if(item.caracteristique.toString().includes(product)) return <div key={item.idProduct}>{item.caracteristique}</div>; 
+          });
+        }
+    }
+
+    useEffect(() => {
+        axios.get("http://localhost:8081/getCluster/"+Cookies.get("iduser")).then((response)=>{
+                setClustering(response.data.reverse());
+            });
+    }, [])
+
+    let itemsToRender;
+    if (clustering) {
+        itemsToRender = clustering.map(item => {
+        return <div key={item.id}>{item.classe}</div>;
+    })};
 
     return (
         <div>
@@ -75,17 +172,33 @@ export default function index() {
                 </p>
             </div>
             <h2>Categorie</h2>
-            <div>
+            <div style={{display:"flex",justifyContent:"space-around"}}>
                 <nav>
                     <ul>
                         <li style={{cursor:'pointer'}} onClick={()=>showTv()}>TV</li>
                         <li style={{cursor:'pointer'}} onClick={()=>showPhones()}>Phone</li>
-                        <li style={{cursor:'pointer'}} onClick={()=>showFitness()}>Fitness</li>
+                        <li style={{cursor:'pointer'}} onClick={()=>showFitness()}>Electromenager</li>
+                        <li style={{cursor:'pointer'}} onClick={()=>showWatchs()}>Montres</li>
+                        <li style={{cursor:'pointer'}} onClick={()=>showParfum()}>Parfum</li>
+                        <li style={{cursor:'pointer'}} onClick={()=>showInfo()}>Informatiques</li>
                     </ul>
                 </nav>
                 <div>
-                    <div style={{border:"1px solid black",height:"300px",overflow:"auto",lineHeight:"30px",width:"800px"}}>
+                    <h2>Produits</h2>
+                    <div style={{border:"1px solid black",height:"300px",overflow:"auto",lineHeight:"30px",width:"500px"}}>
                         <List items={tvs} fallback={"Loading..."} />
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <h2>Clustering</h2>
+                        {itemsToRender}
+                    </div>
+                    <div>
+                        <h2>Produits Recommandes</h2>
+                        <div style={{border:"1px solid black",height:"300px",overflow:"auto",lineHeight:"30px",width:"500px"}}>
+                            <Recommand items={recs} fallback={"Loading..."} />
+                        </div>
                     </div>
                 </div>
             </div>
--- server/server.js
+++ server/server.js
@@ -2,15 +2,22 @@ const express = require("express");
 const app =  express();
 const cors = require("cors");
 var XLSX = require('xlsx');
+const mysql = require("mysql");
 
 app.use(cors());
 app.use(express.json());
+
+const db = mysql.createConnection({
+    user : "root",
+    host : "localhost",
+    password : "",
+    database : "jumiadatabase"
+});
 
 app.get("/getPhones",async(request,response)=>{
     
     var workbook = XLSX.readFile('dataset_android.csv');
     var sheet_name_list = workbook.SheetNames;
-    console.log(XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]))
     response.send(XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]))
 });
 
@@ -18,17 +25,46 @@ app.get("/getTVs",async(request,response)=>{
     
     var workbook = XLSX.readFile('dataset_TV.csv');
     var sheet_name_list = workbook.SheetNames;
-    console.log(XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]))
     response.send(XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]))
 });
 
-app.get("/getFitness",async(request,response)=>{
+app.get("/getElectro",async(request,response)=>{
     
-    var workbook = XLSX.readFile('dataset_fitness.csv');
+    var workbook = XLSX.readFile('dataset_electromenager.csv');
     var sheet_name_list = workbook.SheetNames;
-    console.log(XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]))
     response.send(XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]))
 });
+
+app.get("/getWatchs",async(request,response)=>{
+    
+    var workbook = XLSX.readFile('dataset_watch.csv');
+    var sheet_name_list = workbook.SheetNames;
+    response.send(XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]))
+});
+
+app.get("/getInfo",async(request,response)=>{
+    
+    var workbook = XLSX.readFile('infor_scraper.csv');
+    var sheet_name_list = workbook.SheetNames;
+    response.send(XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]))
+});
+
+app.get("/getParfum",async(request,response)=>{
+    
+    var workbook = XLSX.readFile('parfum_dataset.csv');
+    var sheet_name_list = workbook.SheetNames;
+    response.send(XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]))
+});
+
+app.get("/getCluster/:id",(request,response)=>{
+    db.query("SELECT * FROM `tutorials_cluster` WHERE idUser=? and (classe, date) IN (SELECT classe, Max(date) FROM `tutorials_cluster` GROUP BY classe) ORDER BY date LIMIT 4",[request.params.id],(err,result)=>{
+        if(err){
+            console.log(err);
+        }else{
+            response.send(result);
+        }
+    })
+})
 
 app.listen(8081, () => {
     console.log("Yes, your server is running on port 8081");
